76825f7ef58b70ba008da482106d8360e6a0aada
Tanish Sunita Pareek <tanishpareek2005@gmail.com>
2025-10-08 09:23:23 +0000

    do something its still not loading

--- src/lib/hygraphClient.ts
+++ src/lib/hygraphClient.ts
@@ -1,12 +1,9 @@
 import { GraphQLClient } from 'graphql-request';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const endpoint = 'https://ap-south-1.cdn.hygraph.com/content/cmggglcpv00n807uvfqrgvmz7/master';
 
 export const hygraphClient = new GraphQLClient(endpoint, {
   headers: {
-    Authorization: `Bearer ${process.env.HYGRAPH_CONTENT_API_KEY}`, // store key in .env
+    Authorization: `Bearer ${process.env.HYGRAPH_CONTENT_API_KEY}`,
   },
 });
